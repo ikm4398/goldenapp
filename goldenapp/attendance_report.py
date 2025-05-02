9f5a39036dbe252cd3b1a67adf33158a124d5ec5
Indra Kumar Mehta <ikm4398@gmail.com>
2025-05-02 14:45:05 +0545

    Fix negative time

--- goldenapp/attendance_report.py
+++ goldenapp/attendance_report.py
@@ -1,4 +1,6 @@
 
+#####Fix negative time
+
 import frappe
 from frappe.utils import add_days, nowdate
 
@@ -17,6 +19,18 @@ def create_attendance_query_report():
             "module": "Goldenapp",
             "add_total_row": 1,
             "query": """
+WITH ranked_attendance AS (
+    SELECT
+        att.*,
+        ROW_NUMBER() OVER (
+            PARTITION BY att.employee, att.attendance_date
+            ORDER BY 
+                CASE WHEN att.in_time IS NOT NULL THEN 0 ELSE 1 END,
+                att.modified DESC
+        ) as rn
+    FROM `tabAttendance` att
+    WHERE att.docstatus = 1
+)
 SELECT
     att.employee,
     emp.employee_name,
@@ -25,80 +39,84 @@ SELECT
     att.status,
     att.shift,
     att.in_time,
-    COALESCE(
-        att.out_time,
-        CASE
-            WHEN att.in_time IS NOT NULL AND att.shift = 'Evening' THEN (
-                SELECT ao.out_time
-                FROM `tabAttendance` ao
-                WHERE ao.employee = att.employee
-                  AND ao.attendance_date = att.attendance_date
-                  AND ao.out_time IS NOT NULL
-                  AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
-                  LIMIT 1
-            )
-            WHEN att.in_time IS NOT NULL AND att.shift = 'Morning' THEN (
-                SELECT ao.out_time
-                FROM `tabAttendance` ao
-                WHERE ao.employee = att.employee
-                  AND ao.attendance_date = att.attendance_date
-                  AND ao.out_time IS NOT NULL
-                  AND TIME(ao.out_time) BETWEEN '08:00:00' AND '15:00:00'
-                  LIMIT 1
-            )
-            WHEN att.in_time IS NOT NULL AND att.shift = 'Night' THEN next_att.out_time
-            ELSE NULL
-        END
-    ) AS out_time,
+    CASE
+        WHEN att.out_time IS NOT NULL AND att.out_time > att.in_time THEN att.out_time
+        ELSE (
+            CASE
+                WHEN att.in_time IS NOT NULL AND TIME(att.in_time) BETWEEN '05:00:00' AND '10:00:00' THEN (
+                    SELECT ao.out_time
+                    FROM `tabAttendance` ao
+                    WHERE ao.employee = att.employee
+                      AND ao.attendance_date = att.attendance_date
+                      AND ao.out_time IS NOT NULL
+                      AND ao.out_time > att.in_time
+                      AND TIME(ao.out_time) BETWEEN '08:00:00' AND '15:00:00'
+                    LIMIT 1
+                )
+                WHEN att.in_time IS NOT NULL AND TIME(att.in_time) BETWEEN '12:00:00' AND '17:00:00' THEN (
+                    SELECT ao.out_time
+                    FROM `tabAttendance` ao
+                    WHERE ao.employee = att.employee
+                      AND ao.attendance_date = att.attendance_date
+                      AND ao.out_time IS NOT NULL
+                      AND ao.out_time > att.in_time
+                      AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
+                    LIMIT 1
+                )
+                WHEN att.in_time IS NOT NULL AND TIME(att.in_time) >= '20:00:00' AND next_att.out_time > att.in_time THEN next_att.out_time
+                ELSE NULL
+            END
+        )
+    END AS out_time,
     ROUND(
         CASE
-            WHEN att.in_time IS NOT NULL AND att.out_time IS NOT NULL THEN TIMESTAMPDIFF(SECOND, att.in_time, att.out_time)/3600
-            WHEN att.in_time IS NOT NULL AND att.out_time IS NULL THEN
+            WHEN att.in_time IS NOT NULL THEN
                 TIMESTAMPDIFF(
                     SECOND,
                     att.in_time,
                     COALESCE(
                         CASE
-                            WHEN att.shift = 'Evening' THEN (
+                            WHEN att.out_time IS NOT NULL AND att.out_time > att.in_time THEN att.out_time
+                            WHEN TIME(att.in_time) BETWEEN '05:00:00' AND '10:00:00' THEN (
                                 SELECT ao.out_time
                                 FROM `tabAttendance` ao
                                 WHERE ao.employee = att.employee
                                   AND ao.attendance_date = att.attendance_date
                                   AND ao.out_time IS NOT NULL
-                                  AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
-                                  LIMIT 1
-                            )
-                            WHEN att.shift = 'Morning' THEN (
-                                SELECT ao.out_time
-                                FROM `tabAttendance` ao
-                                WHERE ao.employee = att.employee
-                                  AND ao.attendance_date = att.attendance_date
-                                  AND ao.out_time IS NOT NULL
+                                  AND ao.out_time > att.in_time
                                   AND TIME(ao.out_time) BETWEEN '08:00:00' AND '15:00:00'
-                                  LIMIT 1
+                                LIMIT 1
                             )
-                            WHEN att.shift = 'Night' THEN next_att.out_time
+                            WHEN TIME(att.in_time) BETWEEN '12:00:00' AND '17:00:00' THEN (
+                                SELECT ao.out_time
+                                FROM `tabAttendance` ao
+                                WHERE ao.employee = att.employee
+                                  AND ao.attendance_date = att.attendance_date
+                                  AND ao.out_time IS NOT NULL
+                                  AND ao.out_time > att.in_time
+                                  AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
+                                LIMIT 1
+                            )
+                            WHEN TIME(att.in_time) >= '20:00:00' AND next_att.out_time > att.in_time THEN next_att.out_time
                             ELSE NULL
                         END,
-                        next_att.out_time
+                        NULL
                     )
                 ) / 3600
             ELSE 0
         END, 3
     ) AS working_hours,
     att.late_entry
-FROM
-    `tabAttendance` att
+FROM ranked_attendance att
 LEFT JOIN `tabAttendance` next_att
     ON att.employee = next_att.employee
     AND next_att.attendance_date = DATE_ADD(att.attendance_date, INTERVAL 1 DAY)
     AND next_att.in_time IS NULL
     AND next_att.out_time IS NOT NULL
     AND TIME(next_att.out_time) BETWEEN '08:00:00' AND '10:00:00'
-LEFT JOIN `tabEmployee` emp
-    ON att.employee = emp.employee
+LEFT JOIN `tabEmployee` emp ON att.employee = emp.employee
 WHERE
-    att.docstatus = 1
+    att.rn = 1
     AND att.attendance_date BETWEEN %(from_date)s AND %(to_date)s
     AND (%(employee)s IS NULL OR att.employee = %(employee)s)
 ORDER BY att.employee, att.attendance_date;
