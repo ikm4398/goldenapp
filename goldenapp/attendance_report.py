58c0e0a34a24e1f002b1fd6fa6e27074adf22b9b
Indra Kumar Mehta <ikm4398@gmail.com>
2025-05-02 17:22:12 +0545

    fix only show one record per day to multiple record

--- goldenapp/attendance_report.py
+++ goldenapp/attendance_report.py
@@ -1,6 +1,151 @@
 
-#####Fix negative time
+# import frappe
+# from frappe.utils import add_days, nowdate
 
+# def create_attendance_query_report():
+#     if not frappe.db.exists("Report", "Golden Attendance Report"):
+#         today = nowdate()
+#         from_date = add_days(today, -30)
+#         to_date = today
+
+#         report = frappe.get_doc({
+#             "doctype": "Report",
+#             "report_name": "Golden Attendance Report",
+#             "ref_doctype": "Attendance",
+#             "report_type": "Query Report",
+#             "is_standard": "No",
+#             "module": "Goldenapp",
+#             "add_total_row": 1,
+#             "query": """
+# WITH ranked_attendance AS (
+#     SELECT
+#         att.*,
+#         ROW_NUMBER() OVER (
+#             PARTITION BY att.employee, att.attendance_date
+#             ORDER BY 
+#                 CASE WHEN att.in_time IS NOT NULL THEN 0 ELSE 1 END,
+#                 att.modified DESC
+#         ) as rn
+#     FROM `tabAttendance` att
+#     WHERE att.docstatus = 1
+# )
+# SELECT
+#     att.employee,
+#     emp.employee_name,
+#     att.attendance_date,
+#     att.attendance_day,
+#     att.status,
+#     att.shift,
+#     att.in_time,
+#     CASE
+#         WHEN att.out_time IS NOT NULL AND att.out_time > att.in_time THEN att.out_time
+#         ELSE (
+#             CASE
+#                 WHEN att.in_time IS NOT NULL AND TIME(att.in_time) BETWEEN '05:00:00' AND '10:00:00' THEN (
+#                     SELECT ao.out_time
+#                     FROM `tabAttendance` ao
+#                     WHERE ao.employee = att.employee
+#                       AND ao.attendance_date = att.attendance_date
+#                       AND ao.out_time IS NOT NULL
+#                       AND ao.out_time > att.in_time
+#                       AND TIME(ao.out_time) BETWEEN '08:00:00' AND '15:00:00'
+#                     LIMIT 1
+#                 )
+#                 WHEN att.in_time IS NOT NULL AND TIME(att.in_time) BETWEEN '12:00:00' AND '17:00:00' THEN (
+#                     SELECT ao.out_time
+#                     FROM `tabAttendance` ao
+#                     WHERE ao.employee = att.employee
+#                       AND ao.attendance_date = att.attendance_date
+#                       AND ao.out_time IS NOT NULL
+#                       AND ao.out_time > att.in_time
+#                       AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
+#                     LIMIT 1
+#                 )
+#                 WHEN att.in_time IS NOT NULL AND TIME(att.in_time) >= '20:00:00' AND next_att.out_time > att.in_time THEN next_att.out_time
+#                 ELSE NULL
+#             END
+#         )
+#     END AS out_time,
+#     ROUND(
+#         CASE
+#             WHEN att.in_time IS NOT NULL THEN
+#                 TIMESTAMPDIFF(
+#                     SECOND,
+#                     att.in_time,
+#                     COALESCE(
+#                         CASE
+#                             WHEN att.out_time IS NOT NULL AND att.out_time > att.in_time THEN att.out_time
+#                             WHEN TIME(att.in_time) BETWEEN '05:00:00' AND '10:00:00' THEN (
+#                                 SELECT ao.out_time
+#                                 FROM `tabAttendance` ao
+#                                 WHERE ao.employee = att.employee
+#                                   AND ao.attendance_date = att.attendance_date
+#                                   AND ao.out_time IS NOT NULL
+#                                   AND ao.out_time > att.in_time
+#                                   AND TIME(ao.out_time) BETWEEN '08:00:00' AND '15:00:00'
+#                                 LIMIT 1
+#                             )
+#                             WHEN TIME(att.in_time) BETWEEN '12:00:00' AND '17:00:00' THEN (
+#                                 SELECT ao.out_time
+#                                 FROM `tabAttendance` ao
+#                                 WHERE ao.employee = att.employee
+#                                   AND ao.attendance_date = att.attendance_date
+#                                   AND ao.out_time IS NOT NULL
+#                                   AND ao.out_time > att.in_time
+#                                   AND TIME(ao.out_time) BETWEEN '14:00:00' AND '21:00:00'
+#                                 LIMIT 1
+#                             )
+#                             WHEN TIME(att.in_time) >= '20:00:00' AND next_att.out_time > att.in_time THEN next_att.out_time
+#                             ELSE NULL
+#                         END,
+#                         NULL
+#                     )
+#                 ) / 3600
+#             ELSE 0
+#         END, 3
+#     ) AS working_hours,
+#     att.late_entry
+# FROM ranked_attendance att
+# LEFT JOIN `tabAttendance` next_att
+#     ON att.employee = next_att.employee
+#     AND next_att.attendance_date = DATE_ADD(att.attendance_date, INTERVAL 1 DAY)
+#     AND next_att.in_time IS NULL
+#     AND next_att.out_time IS NOT NULL
+#     AND TIME(next_att.out_time) BETWEEN '08:00:00' AND '10:00:00'
+# LEFT JOIN `tabEmployee` emp ON att.employee = emp.employee
+# WHERE
+#     att.rn = 1
+#     AND att.attendance_date BETWEEN %(from_date)s AND %(to_date)s
+#     AND (%(employee)s IS NULL OR att.employee = %(employee)s)
+# ORDER BY att.employee, att.attendance_date;
+#             """,
+#             "filters": [
+#                 {
+#                     "fieldname": "employee",
+#                     "label": "Employee",
+#                     "fieldtype": "Link",
+#                     "options": "Employee",
+#                     "reqd": 0,
+#                     "default": "HR-EMP-00001"
+#                 },
+#                 {
+#                     "fieldname": "from_date",
+#                     "label": "From Date",
+#                     "fieldtype": "Date",
+#                     "default": from_date,
+#                     "reqd": 1
+#                 },
+#                 {
+#                     "fieldname": "to_date",
+#                     "label": "To Date",
+#                     "fieldtype": "Date",
+#                     "default": to_date,
+#                     "reqd": 1
+#                 }
+#             ]
+#         })
+#         report.insert(ignore_permissions=True)
+#         frappe.db.commit()
 import frappe
 from frappe.utils import add_days, nowdate
 
@@ -19,18 +164,6 @@ def create_attendance_query_report():
             "module": "Goldenapp",
             "add_total_row": 1,
             "query": """
-WITH ranked_attendance AS (
-    SELECT
-        att.*,
-        ROW_NUMBER() OVER (
-            PARTITION BY att.employee, att.attendance_date
-            ORDER BY 
-                CASE WHEN att.in_time IS NOT NULL THEN 0 ELSE 1 END,
-                att.modified DESC
-        ) as rn
-    FROM `tabAttendance` att
-    WHERE att.docstatus = 1
-)
 SELECT
     att.employee,
     emp.employee_name,
@@ -107,7 +240,7 @@ SELECT
         END, 3
     ) AS working_hours,
     att.late_entry
-FROM ranked_attendance att
+FROM `tabAttendance` att
 LEFT JOIN `tabAttendance` next_att
     ON att.employee = next_att.employee
     AND next_att.attendance_date = DATE_ADD(att.attendance_date, INTERVAL 1 DAY)
@@ -116,10 +249,10 @@ LEFT JOIN `tabAttendance` next_att
     AND TIME(next_att.out_time) BETWEEN '08:00:00' AND '10:00:00'
 LEFT JOIN `tabEmployee` emp ON att.employee = emp.employee
 WHERE
-    att.rn = 1
+    att.docstatus = 1
     AND att.attendance_date BETWEEN %(from_date)s AND %(to_date)s
     AND (%(employee)s IS NULL OR att.employee = %(employee)s)
-ORDER BY att.employee, att.attendance_date;
+ORDER BY att.employee, att.attendance_date, att.in_time;
             """,
             "filters": [
                 {
