042c1a21e112382a523947dd8630ca00d4c89379
deskgoo2024 <deskgoo2024@gmail.com>
2025-09-08 13:55:04 +0545

    add previous change core to customapp

---
+++ goldenapp/goldenapp/doctype/ambulance/ambulance.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, ik and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Ambulance(Document):
+	pass
