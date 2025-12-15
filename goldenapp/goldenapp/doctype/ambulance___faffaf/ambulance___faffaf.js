1da6c01ad0f982d8774fa7a860aab80704af18b3
deskgoo2024 <deskgoo2024@gmail.com>
2025-12-15 13:48:05 +0545

    distroyee app

--- goldenapp/goldenapp/doctype/ambulance___faffaf/ambulance___faffaf.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, ik and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Ambulance   faffaf", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- goldenapp/goldenapp/doctype/xray/xray.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, ik and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Xray", {
-// 	refresh(frm) {
-
-// 	},
-// });
